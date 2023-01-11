8d04b78c6ffce4bc11f3c64099590abe69a2b70e
speedroid <speedroid971009@gmail.com>
2023-01-11 17:14:05 +0900

    feat: ViewBinding 적용

--- app/src/main/java/com/survivalcoding/noteapp/presentation/MainActivity.kt
+++ app/src/main/java/com/survivalcoding/noteapp/presentation/MainActivity.kt
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
 import com.survivalcoding.noteapp.R
+import com.survivalcoding.noteapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     private val viewModel: MainViewModel by viewModels { MainViewModel.Factory }
 
     override fun onCreate(savedInstanceState: Bundle?) {
